398b856b5097ed966730ae88452fe0d503f9cd1d
Timite54 <kadernaho@gmail.com>
2025-07-19 17:07:42 +0000

    fix error for import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import AuthContext from "./context/AuthContext.jsx";
+import AuthContext from './context/AuthContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <AuthContext>
